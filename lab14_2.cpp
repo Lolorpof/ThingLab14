c4a1155a5fdb9c434df472e0f695419c38c39788
Lolorpof <sirawit_sir@cmu.ac.th>
2023-01-13 16:46:13 +0700

    lab14_2 completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,31 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool mark[][M]){
+    cout << "------------------------------------------------------------------------\n";
+    for(int i = 0; i < N; i++){
+        cout << "|";
+        for(int j = 0; j < M; j++){
+            if(mark[i][j] == 1){
+                    cout << "*";
+            }
+            else{
+                 cout << " ";
+            }
+        }
+        cout << "|\n";
+    }
+    cout << "------------------------------------------------------------------------";
+}
+
+void updateImage(bool mark[][M], int s, int x, int y){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            double temp = sqrt(pow(i - x, 2) + pow(j - y, 2));
+            if(temp <= (double) s-1){
+                mark[i][j] = 1;
+            }
+        }
+    }
+}
